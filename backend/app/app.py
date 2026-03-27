213c2779341c2f0080b1a03395ecfc5588f8d941
comnk <vhsiao36@gmail.com>
2026-03-27 12:43:59 -0700

    starter code for frontend dashboard, FastAPI code which is supposed to connect to Yahoo Finance but is throwing a 429 error as of now

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import market_routes
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(market_routes.router)
+
 
 @app.get("/")
 async def root():
